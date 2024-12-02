7ae87063c9adf4d163d8be36a7ad73f32b43af93
Sage Choi <sachoi@apollo.com>
2024-12-01 22:04:49 -0500

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,12 +1,21 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        input.forEach { it.println() }
-        return 1
+
+    fun part1(input: List<String>): Long {
+        val (a, b) = input.map {
+            it.split(" ").let { it.first().toLong() to it.last().toLong() }
+        }.unzip()
+        return a.sorted().zip(b.sorted()).map { (x, y) -> abs(x - y) }.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        input.forEach { it.println() }
-        return 1
+    fun part2(input: List<String>): Long {
+        val (a, b) = input.map {
+            it.split(" ").let { it.first().toLong() to it.last().toLong() }
+        }.unzip()
+        val bMap = mutableMapOf<Long, Int>()
+        b.forEach { bMap[it] = 1 + (bMap[it] ?: 0) }
+        return a.map { it * (bMap[it] ?: 0) }.sum()
     }
 
     val input = readInput("Day01")
